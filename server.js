f82cb49a9b6355fdaba64cd362b88e5a14cdeff9
luizgustavoou <luizgustavoou>
2025-01-24 14:25:34 -0300

    feat: :sparkles: finalizar codigo utilizando o package ws

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+const { v4: uuidv4 } = require('uuid'); 
+
 const port = process.env.PORT || 3000;
 const http = require("http");
-const socketIo = require("socket.io");
+const WebSocket = require("ws");
 const redis = require("redis");
 
 const server = http.createServer();
-const io = socketIo(server);
+
+const wss = new WebSocket.Server({ server });
 
 const redisClient = redis.createClient({
     url: "redis://redis:6379",
@@ -15,63 +18,95 @@ const redisSubscriber = redisClient.duplicate();
 redisClient.connect();
 redisSubscriber.connect();
 
-io.on("connection", (socket) => {
-    console.log("Usuário conectado: " + socket.id);
+const userSocketMap = {};
 
-    socket.on("sendChatMessage", (data) => {
-        const { chatId, message } = data;
+const sendMessageToRoom = async (room, message) => {
+    const socketsId = await redisClient.sMembers(room);
+    console.log("[chat-messages] socketsId", socketsId);
 
-        redisClient.publish(
-            "chat-messages",
-            JSON.stringify({ chatId, message })
-        );
+    for (const socketId of socketsId) {
+        const socket = userSocketMap[socketId];
+        if (socket) {
+            console.log(
+                `[chat-messages] enviando mensagem para o socket de id ${socketId}`
+            );
+            socket.send(JSON.stringify({ type: "newMessage", message }));
+        }
+    }
+};
+
+wss.on("connection", (ws) => {
+    const clientId = uuidv4();
+    console.log(`Novo cliente conectado com id: ${clientId}`);
+
+    ws.clientId = clientId;
+
+    userSocketMap[clientId] = ws;
+
+    ws.on("message", (data) => {
+        const parsedData = JSON.parse(data);
+
+        switch (parsedData.type) {
+            case "sendChatMessage":
+                const { chatId, message } = parsedData;
+                redisClient.publish(
+                    "chat-messages",
+                    JSON.stringify({ chatId, message })
+                );
+                break;
+
+            case "enterChat":
+                const { chatId: enterChatId } = parsedData;
+                const room = `chat-${enterChatId}`;
+                console.log(
+                    `[enterChat] Socket ${ws.clientId} entrando na sala ${room}`
+                );
+
+                redisClient.sAdd(room, ws.clientId);
+                break;
+
+            case "leaveChat":
+                const { chatId: leaveChatId } = parsedData;
+                const roomLeave = `chat-${leaveChatId}`;
+                console.log(
+                    `[leaveChat] Socket ${ws.clientId} saindo da sala ${roomLeave}`
+                );
+
+                ws.leave(roomLeave);
+                redisClient.sRem(roomLeave, ws.clientId);
+                break;
+
+            default:
+                console.log("Tipo de mensagem não reconhecido.");
+        }
     });
 
-    socket.on("enterChat", (data) => {
-        console.log("[enterChat] ", data);
-
-        const { chatId } = data;
-
-        const room = `chat-${chatId}`;
-
-        console.log(`[enterChat] Socket ${socket.id} entrando na sala ${room}`);
-        redisClient.sAdd(room, socket.id);
+    // Salvar o WebSocket de cada cliente para mapeamento
+    ws.on("close", () => {
+        for (let userId in userSocketMap) {
+            if (userSocketMap[userId] === ws) {
+                redisClient.hDel("userSocketMap", userId);
+                delete userSocketMap[userId];
+            }
+        }
     });
 
-    socket.on("leaveChat", (data) => {
-        console.log("[leaveChat] ", data);
-
-        const { chatId } = data;
-
-        const room = `chat-${chatId}`;
-
-        console.log(`[leaveChat] Socket ${socket.id} saindo da sala ${room}`);
-
-        socket.leave(room);
-        redisClient.sRem(room, socket.id);
+    ws.on("register", (userId) => {
+        console.log(`Usuário registrado: ${userId}`);
+        userSocketMap[userId] = ws;
+        redisClient.hSet("userSocketMap", userId, ws.clientId);
     });
 });
 
-redisSubscriber.subscribe("chat-messages", async (data, channel) => {
+redisSubscriber.subscribe("chat-messages", async (data) => {
     console.log("[chat-messages] data", data);
 
     const { chatId, message: chatMessage } = JSON.parse(data);
-
     const room = `chat-${chatId}`;
 
-    const socketsId = await redisClient.sMembers(room);
-
-    console.log("socketsId ", socketsId);
-
-    for (const socketId of socketsId ?? []) {
-        console.log(
-            `[chat-messages] enviarei mensagem para o socket de id ${socketId}`
-        );
-
-        io.to(socketId).emit("newMessage", chatMessage);
-    }
+    await sendMessageToRoom(room, chatMessage);
 });
 
 server.listen(port, () => {
-    console.log("Servidor Socket.IO rodando na porta ", port);
+    console.log("Servidor WebSocket rodando na porta ", port);
 });
