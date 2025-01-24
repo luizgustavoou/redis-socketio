5c782bcb23ccef2bd818d5c8f40fa5f13197e647
luizgustavoou <luizgustavoou>
2025-01-24 13:06:13 -0300

    feat: :sparkles: usar package redis atualizado

--- server.js
+++ server.js
@@ -3,15 +3,29 @@ const http = require("http");
 const socketIo = require("socket.io");
 const redis = require("redis");
 
-// Criação do servidor HTTP e do Socket.IO
 const server = http.createServer();
 const io = socketIo(server);
 
-const redisClient = redis.createClient({ host: "redis", port: 6379 });
+const redisClient = redis.createClient({
+    url: "redis://redis:6379",
+});
+
 const redisSubscriber = redisClient.duplicate();
-redisSubscriber.subscribe("user-messages");
+
+redisClient.connect();
+redisSubscriber.connect();
+
+redisClient.on("connect", () => {
+    console.log("Redis is ready!");
+});
+
+redisClient.on("error", (e) => {
+    console.log("Redis error ", e);
+});
 
 const userSocketMap = {};
+redisClient.set("key", "value");
+console.log("userSocketMap ", userSocketMap);
 
 io.on("connection", (socket) => {
     console.log("Usuário conectado: " + socket.id);
@@ -20,10 +34,11 @@ io.on("connection", (socket) => {
         console.log(`Usuário registrado: ${userId}`);
         userSocketMap[userId] = socket.id;
 
-        redisClient.hset("userSocketMap", userId, socket.id);
+        console.log("[REGISTER] userSocketMap ", userSocketMap);
+
+        redisClient.hSet("userSocketMap", userId, socket.id);
     });
 
-    // Enviar mensagem para um usuário específico
     socket.on("sendMessage", (data) => {
         const { userId, message } = data;
         const targetSocketId = userSocketMap[userId];
@@ -40,7 +55,26 @@ io.on("connection", (socket) => {
         }
     });
 
-    // Quando o socket desconectar
+    socket.on("sendChatMessage", (data) => {
+        const { chatId, message } = data;
+
+        redisClient.publish(
+            "chat-messages",
+            JSON.stringify({ chatId, message })
+        );
+    });
+
+    socket.on("enterChat", (data) => {
+        console.log("[enterChat] ", data);
+
+        const { chatId } = data;
+
+        const room = `chat-${chatId}`;
+
+        socket.join(room);
+        redisClient.sadd(room, socket.id);
+    });
+
     socket.on("disconnect", () => {
         console.log("Usuário desconectado: " + socket.id);
         for (let userId in userSocketMap) {
@@ -52,26 +86,28 @@ io.on("connection", (socket) => {
     });
 });
 
-redisSubscriber.on("message", (channel, message) => {
-    console.log("[redisSubscriber]");
-    console.log({
-        channel,
-        message,
-    });
+redisSubscriber.subscribe("user-messages", async (data, channel) => {
+    console.log("[user-messages] data", data);
+    const { userId, message } = JSON.parse(data);
+    const targetSocketId = userSocketMap[userId];
 
-    if (channel === "user-messages") {
-        const { userId, message: userMessage } = JSON.parse(message);
-        const targetSocketId = userSocketMap[userId];
-
-        if (targetSocketId) {
-            io.to(targetSocketId).emit("receiveMessage", userMessage);
-        } else {
-            console.log(`Usuário ${userId} ainda não está conectado.`);
-        }
+    if (targetSocketId) {
+        io.to(targetSocketId).emit("receiveMessage", message);
+    } else {
+        console.log(`Usuário ${userId} ainda não está conectado.`);
     }
 });
 
-// Iniciar o servidor
+redisSubscriber.subscribe("chat-messages", async (data, channel) => {
+    console.log("[chat-messages] data", data);
+
+    const { chatId, message: chatMessage } = JSON.parse(data);
+
+    const socketsId = await redisClient.sMembers(`chat-${chatId}`);
+
+    console.log("socketsId ", socketsId);
+});
+
 server.listen(port, () => {
     console.log("Servidor Socket.IO rodando na porta ", port);
 });
